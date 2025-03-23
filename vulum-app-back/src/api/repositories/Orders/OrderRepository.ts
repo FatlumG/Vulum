e9b45efddcfe04f4c4cabfffac303d700ee7493f
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-03-23 11:42:53 +0100

    Completed orders

---
+++ vulum-app-back/src/api/repositories/Orders/OrderRepository.ts
@@ -0,0 +1,20 @@
+import { Order } from '@api/models/Orders/Order';
+import { EntityRepository } from 'typeorm';
+import { RepositoryBase } from '@base/infrastructure/abstracts/RepositoryBase';
+
+@EntityRepository(Order)
+export class OrderRepository extends RepositoryBase<Order> {
+  public async createOrder(data: object) {
+    let entity = new Order();
+
+    Object.assign(entity, data);
+
+    return await this.save(entity);
+  }
+
+  public async updateOrder(Order: Order, data: object) {
+    Object.assign(Order, data);
+
+    return await Order.save(data);
+  }
+}
---
+++ vulum-app-back/src/api/requests/Orders/OrderCreateRequest.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class OrderCreateRequest {
+  @IsNotEmpty()
+  @IsString()
+  OName: string;
+
+  @IsNotEmpty()
+  UserId: number;
+
+  @IsNotEmpty()
+  TotalPrice: number;
+}
