ae1fb70a403171f52c5466d8632fdf66d4bcb939
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-05-30 21:47:44 +0200

    Consume api data for dashboard&header

---
+++ vulum-app-front/src/interfaces/UserInterface.ts
@@ -0,0 +1,20 @@
+export interface userInterface {
+  Address: string;
+  Email: string;
+  FName: string;
+  Favorites: number;
+  FullName: string;
+  LName: string;
+  Orders: number;
+  Payments: number;
+  Pendings: number;
+  Phone: string;
+  PricingPlan: object;
+  Products: number;
+  ProfilePhotoUrl: null;
+  role: any;
+  Sales: number;
+  Todos: number;
+  Username: string;
+  id: number;
+}
--- vulum-app-front/src/pages/DashboardPage.tsx
+++ vulum-app-front/src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, FC } from "react";
 import Header from "../components/Header";
 import DashboardSidebar from "../components/DashboardSidebar";
 import TotalStats from "../components/TotalStats";
@@ -6,50 +6,68 @@ import users from "../assets/figures/users.svg";
 import orders from "../assets/figures/orders.svg";
 import Sales from "../assets/figures/sales.svg";
 import pendings from "../assets/figures/pendings.svg";
+import api from "../auth/api";
+import { userInterface } from "../interfaces/UserInterface";
 
-const DashboardPage: React.FC = () => {
+const DashboardPage: FC = () => {
+  const [user, setUser] = useState<userInterface>();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await api.get("/users/me");
+        setUser(res.data);
+        console.log(res.data, "res.data");
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="grid grid-cols-12">
         <DashboardSidebar classes="col-span-2" />
         <div className="col-span-10 py-5 px-10 font-NunitoSans">
-          <h1 className="text-[32px] font-bold">Dashboard</h1>
+          <h1 className="text-[32px] font-bold">Your Dashboard</h1>
           <div className="flex items-center gap-10">
             <TotalStats
               img={users}
               alt="users"
               title="Total Users"
-              quantity="40,689"
+              quantity={40689}
               percentage="8,5%"
               descr="Up from yesterday"
               up={true}
             />
             <TotalStats
               img={orders}
-              alt="orders"
-              title="Total Orders"
-              quantity="10293"
-              percentage="1,3%"
-              descr="Up from past week"
+              alt="products"
+              title="Total Products"
+              quantity={user?.Products}
+              percentage="1,8%"
+              descr="Up from yesterday"
               up={true}
             />
             <TotalStats
               img={Sales}
               alt="sales"
               title="Total Sales"
-              quantity="$89,000"
+              quantity={user?.Sales}
               percentage="4,3%"
               descr="Up from yesterday"
               up={false}
             />
             <TotalStats
               img={pendings}
-              alt="pendings"
-              title="Total Pendings"
-              quantity="2040"
-              percentage="1,8%"
-              descr="Up from yesterday"
+              alt="orders"
+              title="Total Orders"
+              quantity={user?.Orders}
+              percentage="1,3%"
+              descr="Up from past week"
               up={true}
             />
           </div>
